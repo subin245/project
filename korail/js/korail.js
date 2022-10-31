5cb2a8b910566842950924982ee135da38461580
subin245 <114711179+subin245@users.noreply.github.com>
2022-10-31 08:25:31 +0000

    re

--- korail/js/korail.js
+++ korail/js/korail.js
@@ -1,3 +1,4 @@
+// sec01 이벤트 베너
 var swiper = new Swiper(".mySwiper01", {
     slidesPerView: 1,
     spaceBetween: 0,
@@ -12,8 +13,18 @@ var swiper = new Swiper(".mySwiper01", {
     },
   });
 
-
+// sec02 관광열차
   var swiper = new Swiper(".mySwiper02", {
+    slidesPerView: 6,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+// sec03 자유여행
+  var swiper = new Swiper(".mySwiper03", {
     slidesPerView: "auto",
     spaceBetween: 20,
     pagination: {
