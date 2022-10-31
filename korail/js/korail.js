314d0585eb319ceffb5c433c641396729066ceda
subin245 <114711179+subin245@users.noreply.github.com>
2022-10-31 07:01:54 +0000

    re

---
+++ korail/js/korail.js
@@ -0,0 +1,22 @@
+var swiper = new Swiper(".mySwiper01", {
+    slidesPerView: 1,
+    spaceBetween: 0,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+
+  var swiper = new Swiper(".mySwiper02", {
+    spaceBetween: 20,
+    pagination: {
+      el: ".swiper-pagination",
+      dynamicBullets: true,
+    },
+  });
