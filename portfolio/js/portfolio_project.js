0bf7c4de7e20da0a8790259ccdb59e86ca0d83b2
subin245 <114711179+subin245@users.noreply.github.com>
2022-11-19 20:03:27 +0900

    re

--- portfolio/js/portfolio_project.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-    $(".sec02 .card").mouseenter(function(){
-        $(this).children(".txt").addClass("on");
-    });
-    $(".sec02 .card").mouseleave(function(){
-        $(".sec02 .card .txt").removeClass("on");
-    });
-});
